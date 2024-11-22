400e4eb907284e1cde3c03f23c80dbdadf4de7b8
Cplus Shen <cplus.shen@gmail.com>
2024-11-22 00:19:12 +0000

    added log for sync.

--- git-repo-sync.sh
+++ git-repo-sync.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -xe
+#
 GIT_DIR=/home/git
+LOG=${GIT_DIR}/log/or_$(date +%F).log
 
 cd $GIT_DIR
 
 for i in $(cat projects.list); do
-  echo $i
+  echo $i | tee -a $LOG
   cd $i
-  git fetch
+  git fetch | tee -a $LOG
   cd ..
 done
