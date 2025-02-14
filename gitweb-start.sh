a5977b133ce8a92f0ab173186f18f34f3d1b305a
Cplus Shen <cplus.shen@gmail.com>
2025-02-14 08:35:06 +0800

    update gitweb start script.

--- gitweb-start.sh
+++ gitweb-start.sh
@@ -1,13 +1,4 @@
-/usr/bin/docker run \
-	-d \
-	--name repoweb \
-	-e PROJECTROOT=/var/lib/git \
-	-e PROJECTS_LIST=/var/lib/git/projects.list \
-	-v /home/git:/var/lib/git:ro \
-	-v /etc/localtime:/etc/localtime:ro \
-	-p 8081:80 \
-	mlan/gitweb
+#!/bin/bash
+/usr/bin/docker start repoweb
 
-#/usr/bin/docker start repoweb
-
-#/usr/bin/docker update --restart unless-stopped repoweb
+exit 0
