145a7a7e6a0d3dd8355cce0993371accf88e2d77
Cplus Shen <cplus.shen@gmail.com>
2025-04-17 15:10:59 +0800

    backup for bitbucket projects.

--- git-repo-sync.sh
+++ git-repo-sync.sh
@@ -4,11 +4,13 @@ GIT_DIR=/home/git
 LOG=${GIT_DIR}/log/git_sync_$(date +%F).log
 
 date | tee -a $LOG
+source /home/git/git-repo-utils/git-env
 
 cd $GIT_DIR
 for i in $(cat projects.list); do
   echo $i | tee -a $LOG
   cd $i
   git fetch 2>&1 | tee -a $LOG
+  git remote update 2>&1 | tee -a $LOG
   cd ..
 done
