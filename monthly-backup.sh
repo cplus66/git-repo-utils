b95311d2079f2c1f1ff244195a2a56233d57e580
Cplus Shen <cplus.shen@gmail.com>
2025-04-17 15:15:42 +0800

    get project list from bitbucket.txt.

--- monthly-backup.sh
+++ monthly-backup.sh
@@ -3,33 +3,7 @@
 GIT_HOME=/home/git
 
 # List changed project list
-LIST="
-abar_5001_jetpack_5.1
-adv_bmi_088
-adv_bmp_390
-adv_gmsl
-framework_ria
-gs3-bsp
-jetson_bsp_web
-mic_711_jetpack_6.2.0
-mic_711on_8g_jetpack_6.2.0
-mic_711on_jetpack_6.2.0
-mic_712_jetpack_6.1.0
-mic_712_jetpack_6.2.0
-mic_713_jetpack_5.1.2
-mic_713_jetpack_6.1.0
-mic_713_jetpack_6.2.0
-mic_713on_8g_jetpack_6.2.0
-mic_715ox_jetpack_5.1.2
-mic_732_64g_jetpack_6.0
-mic_732_64g_jetpack_6.2
-mic_733ao_64g_jetpack_6.0
-mic_file_source
-mic_tester
-rootfs_bin_adv
-vega_dud550_ca_jetpack_6.1.0
-vega_dud550_jetpack_6.1_spe
-"
+LIST=$(cat bitbucket.txt)
 
 # Clone if the repositories doesn't exist.
 cd $GIT_HOME
